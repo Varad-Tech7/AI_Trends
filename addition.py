a0b7a703ca0319538ea72fab56770c70d3d2e8b7
unknown <varadkulkarni003@gmail.com>
2026-06-28 22:12:39 +0530

    Multiplication function added

--- addition.py
+++ addition.py
@@ -1,6 +1,7 @@
 fn_add = lambda x,y : x+y
+fn_multiply = lambda x,y : x*y
 
 a = int(input("Enter a number: "))
 b = int(input("Enter another number: "))
 print("The sum is : ",fn_add(a,b))
-
+print("The multiplication is: ",fn_multiply(a,b))
